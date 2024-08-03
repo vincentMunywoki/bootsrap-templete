e40f18f0f24f66c19916fc873535a10cd4f1d168
vincent munywoki <vincentmunywoki12@gmail.com>
2024-08-03 14:27:48 +0300

    initial commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    //Hero Slider.
+    $('#hero-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items: 1,
+        dots: false,
+        smartSpeed:1000,
+        navText: ['PREV', 'NEXT'],
+        responsive:{
+            0:{
+                
+            },
+            600:{
+                
+            },
+            1000:{
+                
+            }
+        }
+    })
+})
